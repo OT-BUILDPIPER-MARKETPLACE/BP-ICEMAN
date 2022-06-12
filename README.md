7a426adfaf4a27b0fd19f2ee2c7cd8fa807055e1
Rajat vats <rajat.vats@opstree.com>
2022-06-12 11:59:36 +0530

    Added feature to stop/start K8s deployment and statefulsets based on annotations placed on them.

--- README.md
+++ README.md
@@ -2,35 +2,45 @@
 
 # BP-ICEMAN 
 
-As **ICEMAN** has capability to freeze objects, similarly I'll help to freeze/unfreeze AWS resources by stopping/starting it based on user-defined tags and user-defined actions.
+As **ICEMAN** has capability to freeze objects, similarly I'll help to freeze/unfreeze AWS resources by stopping/starting it based on user-defined tags and user-defined actions and increase or descrease the replica count of the K8s resources(Deployment and Statefulset) on the basis of annotation.
 
 ## SERVICES SUPPORTED
 - EC2
 - RDS
+- k8s
+    - Deployment
+    - Statefulset
 
-## CONFIGURATIONS 
-Configuration for this utility will be managed in YAML format. Below are the configurations details :
 
-- ***aws_profile (Optional) :*** It is a aws profile that you can use to a perform start/stop resources actions in AWS. If not specified, then utility will use default credentials.
-
-- ***aws_region (Optional) :*** The AWS Region where this utility is executed.
-
-- ***ec2_tags (Optional) :*** Tags given to ec2 instances.This utility will start/stop all the ec2 instances matches to this given tags.
-
-- ***rds_tags (Optional) :*** Tags given to rds .It will start or stop all the rds matches to this given tags.
 
 ## SAMPLE CONF FILE
 
 ```
-aws_profile: default
 
-region: us-east-1
+aws:
+  ec2_tags:
+    start_stop_schedule : "true"
+  region: 
+  rds_tags:
+    start_stop_schedule : "true"
+  aws_profile: 
 
-ec2_tags:
-  start_stop_schedule : "true"
+k8s: 
+  context: 
+  namespace: 
+  replicas: 
+  deployment_annotations:
+    start_stop_schedule : "true"
+  sts_annotations:
+    start_stop_schedule : "true"
 
-rds_tags:
-  start_stop_schedule : "true"
+actions_on:
+    - k8s:
+        - deployment
+        - sts
+    
+    - aws:
+        - ec2
 ```
 
 ## USAGE
@@ -40,37 +50,24 @@ rds_tags:
 ### LOCALLY
 To run this utility locally from your system.Follow below steps.
 - Clone this repo.
-- Create your configuration file. Can take reference from ```config/schedule_resources_sample.yml```.
+- Create your configuration file. Can take reference from ```config/schedule_resources_sample_config.yml```.
 - Export two environment variables .
    - ```CONF_PATH:``` Path of the configuration file.
    
-   - ```SCHEDULE_ACTION:``` Action which is  going to perform i.e start or stop.
+   - ```AWS_SCHEDULE_ACTION:``` Action which is  going to perform i.e start or stop.
 
+   - ```K8s_SCHEDULE_ACTION:``` Action which is  going to perform i.e resize.
    ```
-   export CONF_PATH="/home/opstree/BP-ICEMAN/config/schedule_resources_sample.yml"
+   export CONF_PATH="${pwd}/BP-ICEMAN/config/schedule_resources_sample_config.yml"
    ```
    ```
-   export SCHEDULE_ACTION="start"
+   export AWS_SCHEDULE_ACTION="start"
+   export K8s_SCHEDULE_ACTION="resize"
+
    ```
 
 - Run the python script.
 
    ```
-   python3 scripts/schedule_resources.py 
+   python3 scripts/schedule_resource_factory.py 
    ```
-
-### USING DOCKER
-To run this utility using docker.Follow below steps.
-- Clone this repo.
-- Make changes to configuration files as required i.e config/schedule_resources.yml.
-- Build the docker image using Makefile
-  ```
-   make build VERSION=<Provide image tag here>
-  ```
-  ![make_build](images/make_build.png)
-
-- Run the application
-  ```
-  make run VERSION=<Provide build image tag> CONF_PATH=<Conf-Path> ACTION=<start/stop>
-  ```
-  ![make_run](images/make_run.png) 
---
+++ scripts/aws_resources.py
@@ -0,0 +1,137 @@
+import os
+import schedule_resource_logger
+from otawslibs import generate_aws_session , aws_resource_tag_factory , aws_ec2_actions_factory , aws_rds_actions_factory
+from botocore.exceptions import ClientError
+
+SCHEULE_ACTION_ENV_KEY = "AWS_SCHEDULE_ACTION"
+LOG_PATH = "/var/log/ot/aws-resource-scheduler.log"
+
+LOGGER = schedule_resource_logger._get_logging(LOG_PATH)
+
+
+def _fetch_instance_ids(client, service, tags):
+    
+    aws_resource_finder = aws_resource_tag_factory.getResoruceFinder(client,service)
+    instance_ids = aws_resource_finder._get_resources_using_tags(tags)
+            
+    return instance_ids
+
+def _schedule_ec2(properties, session , args):
+
+    for property in properties['aws']:
+
+        if property == "ec2_tags":
+            
+            LOGGER.info(f'Reading Ec2 tags')
+
+            ec2_tags = properties['aws']['ec2_tags']
+            
+            if ec2_tags:
+
+                LOGGER.info(f'Found Ec2 tags details for filtering : {ec2_tags}')
+
+                ec2_client = session.client("ec2", region_name=properties['aws']['region'])
+                aws_ec2_action = aws_ec2_actions_factory.awsEC2Actions(ec2_client)
+
+
+                LOGGER.info(f'Scanning AWS EC2 resources in {properties["aws"]["region"]} region based on tags {ec2_tags} provided')
+
+                instance_ids = _fetch_instance_ids(ec2_client, "ec2", ec2_tags)
+
+                if instance_ids:
+
+                    LOGGER.info(f'Found AWS EC2 resources {instance_ids} in  {properties["aws"]["region"]} region based on tags provided: {ec2_tags}',extra={"ec2_ids": instance_ids})
+
+                    if os.environ[SCHEULE_ACTION_ENV_KEY] == "start":
+                        
+                        aws_ec2_action._ec2_perform_action(instance_ids,action="start")
+
+                    elif os.environ[SCHEULE_ACTION_ENV_KEY] == "stop":
+
+                        aws_ec2_action._ec2_perform_action(instance_ids,action="stop")
+
+                    else:
+                        logging.error(f"{SCHEULE_ACTION_ENV_KEY} env not set")
+
+                else:
+                    LOGGER.warning(f'No Ec2 instances found on the basis of tag filters provided in conf file in region {properties["aws"]["region"]} ',extra={"ec2_ids": instance_ids})
+            else:
+                LOGGER.warning(f'Found ec2_tags key in config file but no Ec2 tags details mentioned for filtering',extra={"ec2_ids": instance_ids})
+
+
+def _schedule_rds(properties, session , args):
+
+    for property in properties['aws']:
+        if property == "rds_tags":
+
+            LOGGER.info(f'Reading RDS tags')
+
+            rds_tags = properties['aws']['rds_tags']
+
+            if rds_tags:
+
+                LOGGER.info(f'Found RDS tags details for filtering : {rds_tags}')
+
+                rds_client = session.client("rds", region_name=properties['aws']['region'])
+
+                aws_rds_action = aws_rds_actions_factory.awsRDSActions(rds_client)
+
+                LOGGER.info(f'Scanning AWS RDS resources in {properties["aws"]["region"]} region based on tags {rds_tags} provided')
+
+                instance_ids = _fetch_instance_ids(rds_client, "rds", rds_tags)
+
+                if instance_ids:
+
+                    LOGGER.info(f'Found AWS RDS resources {instance_ids} in  {properties["aws"]["region"]} region based on tags provided: {rds_tags}',extra={"rds_ids": instance_ids})
+
+                    if os.environ[SCHEULE_ACTION_ENV_KEY] == "start":
+
+                        aws_rds_action._rds_perform_action(instance_ids,action="start")
+
+                    elif os.environ[SCHEULE_ACTION_ENV_KEY] == "stop":
+                        
+                        aws_rds_action._rds_perform_action(instance_ids,action="stop")
+
+                    else:
+                        logging.error(f"{SCHEULE_ACTION_ENV_KEY} env not set")
+                
+                else:
+                    LOGGER.warning(f'No RDS instances found on the basis of tag filters provided in conf file in region {properties["aws"]["region"]} ',extra={"rds_ids": instance_ids})
+            else:
+                LOGGER.warning(f'Found rds_tags key in config file but no RDS tags details mentioned for filtering',extra={"rds_ids": instance_ids})
+            
+        else:
+            LOGGER.info("Scanning AWS service details in config")
+
+
+def _scheduleFactory(properties, aws_profile, resource_type , args):
+
+    instance_ids = []
+            
+    try:
+        
+        LOGGER.info(f'Connecting to AWS.')
+
+        if aws_profile:
+            session = generate_aws_session._create_session(aws_profile)
+        else:
+            session = generate_aws_session._create_session()
+
+        LOGGER.info(f'Connection to AWS established.')
+
+        if resource_type == "ec2":
+            _schedule_ec2(properties, session , args)
+        elif resource_type == "rds":
+            _schedule_rds(properties, session , args)
+        else:
+            LOGGER.info(f'Invalid Resource type provided. Valid values: [ec2,rds]') 
+
+    except ClientError as e:
+        if "An error occurred (AuthFailure)" in str(e):
+            raise Exception('AWS Authentication Failure!!!! .. Please mention valid AWS profile in property file or use valid IAM role ').with_traceback(e.__traceback__)    
+        else:
+            raise e
+    except KeyError as e:
+        raise Exception(f'Failed fetching env {SCHEULE_ACTION_ENV_KEY} value. Please add this env variable').with_traceback(e.__traceback__)    
+
+
